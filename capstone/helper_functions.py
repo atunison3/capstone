0b6b51a62bfcaee40e36a6ffcbbab8aa8391987c
atunison3 <atunison1@yahoo.com>
2026-07-12 15:11:59 -0500

    Updated helper_functions.py: added function to get fips data

--- capstone/helper_functions.py
+++ capstone/helper_functions.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import re
+import requests
 import tomllib
 from pandas import DataFrame
 from pathlib import Path
@@ -23,3 +25,25 @@ def load_config(config_path: Path = Path("config.toml")) -> dict:
 
     with config_path.open("rb") as file:
         return tomllib.load(file)
+
+
+def load_fips_data() -> DataFrame:
+    """Loads FIPS data"""
+
+    url = "https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt"
+
+    resp = requests.get(url, timeout=30)
+
+    pattern = r"^\s*(\d{2})\s+([A-Z]+(?:\s+[A-Z]+)*)\s*$"
+
+    matches = re.findall(pattern, resp.text, flags=re.MULTILINE)
+
+    df = pd.DataFrame(matches, columns=["FIPS", "State"])
+
+    return df
+
+
+if __name__ == "__main__":
+
+    fips_df = load_fips_data()
+    print(fips_df.head())
